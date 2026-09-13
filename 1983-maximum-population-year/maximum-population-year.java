aaf0998f3b009b285198e358acb234d6ac8312c4
Aditya Kumar <144610232+Aditya1742@users.noreply.github.com>
2026-09-13 13:30:16 +0530

    Time: 3 ms (20.20%) | Memory: 43.3 MB (73.61%) - LeetSync

---
+++ 1983-maximum-population-year/maximum-population-year.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximumPopulation(int[][] logs) {
+        int maxPopulation = 0;
+        int answerYear = 1950;
+
+        for (int year = 1950; year <= 2050; year++) {
+            int count = 0;
+
+            for (int i = 0; i < logs.length; i++) {
+                int birth = logs[i][0];
+                int death = logs[i][1];
+
+                if (birth <= year && year < death) {
+                    count++;
+                }
+            }
+
+            if (count > maxPopulation) {
+                maxPopulation = count;
+                answerYear = year;
+            }
+        }
+
+        return answerYear;
+    }
+}
